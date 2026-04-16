b2e259df947c49bc2f74f0e8ce0dcc881cbd201d
tmiko001 <tmiko001@ucr.edu>
2026-04-16 00:55:57 +0000

    ?

--- shift_register.sv
+++ shift_register.sv
@@ -1,12 +1,12 @@
 module shift_register (
-    input  logic        clk,
-    input  logic        rst,
-    input  logic        data,
-    input  logic        wr_en,
+    input  logic clk,
+    input  logic rst,
+    input  logic data,
+    input  logic wr_en,
     output logic [31:0] out
 );
     parameter DIRECTION = 1;
-    always_ff @(posedge clk) begin
+    always @(posedge clk) begin
         if (rst) begin
             out <= 32'b0;
         end else if (wr_en) begin
